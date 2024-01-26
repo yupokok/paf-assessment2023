a377402a1fcdb870f8e59ab875ebdc364e533bb5
yupokok <kimberlywsp@gmail.com>
2024-01-26 10:35:23 +0800

    initial commit

---
+++ bedandbreakfastapp/src/main/java/vttp2023/batch4/paf/assessment/repositories/BookingsRepository.java
@@ -0,0 +1,43 @@
+package vttp2023.batch4.paf.assessment.repositories;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import vttp2023.batch4.paf.assessment.models.Bookings;
+import vttp2023.batch4.paf.assessment.models.User;
+
+@Repository
+public class BookingsRepository {
+	
+	// You may add additional dependency injections
+
+	public static final String SQL_SELECT_USER_BY_EMAIL = "select * from users where email like %";
+
+	@Autowired
+	private JdbcTemplate template;
+
+	// You may use this method in your task
+	public Optional<User> userExists(String email) {
+		SqlRowSet rs = template.queryForRowSet(SQL_SELECT_USER_BY_EMAIL, email);
+		if (!rs.next())
+			return Optional.empty();
+
+		return Optional.of(new User(rs.getString("email"), rs.getString("name")));
+	}
+
+	// TODO: Task 6
+	// IMPORTANT: DO NOT MODIFY THE SIGNATURE OF THIS METHOD.
+	// You may only add throw exceptions to this method
+	public void newUser(User user) {
+	}
+
+	// TODO: Task 6
+	// IMPORTANT: DO NOT MODIFY THE SIGNATURE OF THIS METHOD.
+	// You may only add throw exceptions to this method
+	public void newBookings(Bookings bookings) {
+	}
+}
---
+++ data/task1.sql
@@ -0,0 +1,35 @@
+-- Write your Task 1 answers in this file
+create database bedandbreakfast;
+
+use bedandbreakfast;
+
+create table users (
+   email varchar(128) not null,
+   name varchar(128) not null,
+
+   primary key(email)
+);
+
+create table bookings (
+   booking_id char(8) not null, -- ObjectID
+   listing_id varchar(20),
+   duration int not null,
+   email varchar(128) not null,
+
+   primary key(booking_id),
+   foreign key(email) references users(email)
+);
+
+create table reviews (
+   id int auto_increment,
+   date timestamp,
+   listing_id varchar(20),
+   reviewer_name varchar(20) not null,
+   comments text,
+
+   primary key(id)
+);
+
+grant all privileges on bedandbreakfast.* to fred@'%';
+
+flush privileges;
